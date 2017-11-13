f1fdc4b4c914da6d9b23a60561e29ac89c8e969f
Pedro <>
2017-11-13 09:22:11 -0200

    Alteração Banco Servidor

--- coneccao.php
+++ coneccao.php
@@ -1,8 +1,8 @@
 <?php
-define("HOST", "localhost");
-define("USER", "root");
-define("PASS", "");
-define("DB", "projeto");
+define("HOST", "trabalho-bd-php.000webhostapp.com");
+define("USER", "id3601061_admin");
+define("PASS", "admin");
+define("DB", "id3601061_projeto");
 $con = mysqli_connect(HOST,USER,PASS) or die(" when i outside and saw the moom,i breathe and think of you, i'm think of you ooo ");
 $banco = mysqli_select_db($con, DB);
 
